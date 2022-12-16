763c052249f5a5d02def181e40ba203a5981655f
Sevladem <Sevladem@gmail.com>
2022-12-16 17:26:24 +0200

    upd 1

--- options.js
+++ options.js
@@ -2,6 +2,6 @@ module.exports = {'token' : '5744811510:AAEUpUo6JOddlFIhvpRhMcjrcJsMmPnCrOs', 'p
         {
             key: "osbb7b",
             address: '93.175.228.129',
-            port: 8080
+            port: 8000
         }
     ]}
